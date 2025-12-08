5d495bd416f9a03a1bf68a0a8824e01ac3e07331
Ansul Agrawal <ansul.developer@gmail.com>
2025-12-08 17:26:39 +0530

    feat: integrate react-tooltip and add Dock component
    - Add react-tooltip dependency for enhanced tooltip functionality.
    - Introduce Dock component in App for improved UI layout.
    - Update index.css for tooltip styling adjustments.
    - Define dockApps constant for managing dock application icons and states.
    - Completed Dock Ui with animations and tooltip.

--- src/constants/index.js
+++ src/constants/index.js
@@ -38,6 +38,45 @@ const navIcons = [
   },
 ];
 
+const dockApps = [
+  {
+    id: "finder",
+    name: "Portfolio",
+    icon: "finder.png",
+    canOpen: true,
+  },
+  {
+    id: "safari",
+    name: "Safari",
+    icon: "safari.png",
+    canOpen: true,
+  },
+  {
+    id: "photos",
+    name: "Gallery",
+    icon: "photos.png",
+    canOpen: true,
+  },
+  {
+    id: "contact",
+    name: "Contact",
+    icon: "contact.png",
+    canOpen: true,
+  },
+  {
+    id: "terminal",
+    name: "Skills",
+    icon: "terminal.png",
+    canOpen: true,
+  },
+  {
+    id: "trash",
+    name: "Archive",
+    icon: "trash.png",
+    canOpen: false,
+  },
+];
+
 export { FULL_NAME, SHORT_NAME };
 
-export { navLinks, navIcons };
+export { navLinks, navIcons, dockApps };
